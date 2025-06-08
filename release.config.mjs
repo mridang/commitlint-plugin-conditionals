1276131b5f1b45a4b473d2ce41ac849f30f6bb15
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-06-08 18:56:45 +0300

    chore: imporved the nodejs compat constraint, fixed the formatting and and dependabot config

--- release.config.mjs
+++ release.config.mjs
@@ -28,5 +28,6 @@ export default {
       },
     ],
   ],
-  repositoryUrl: "git+https://github.com/mridang/commitlint-plugin-conditionals.git"
+  repositoryUrl:
+    'git+https://github.com/mridang/commitlint-plugin-conditionals.git',
 };
